e9e67018db624c1cf04e863b0564609b35540150
Gary Bowers <garybowers@google.com>
2020-12-08 10:23:22 +0000

    Adding private gke cluster

--- variables.tf
+++ variables.tf
@@ -1,7 +1,47 @@
 variable "project_id" {
+  type        = string
+  description = "The project ID to host the cluster in (required)"
+}
+
+variable "region" {
 
 }
 
-variable "asm_enabled" {
+variable "zones" {
 
 }
+
+variable "nodepool-name" {
+
+}
+
+variable "node_min_count" {
+
+}
+
+variable "node_max_count" {
+
+}
+
+variable "node_initial_count" {
+
+}
+
+
+variable "vpc_network" {
+
+}
+
+variable "subnetwork" {
+
+}
+
+variable "ip_range_pods" {
+
+}
+
+variable "ip_range_services" {
+
+}
+
+
