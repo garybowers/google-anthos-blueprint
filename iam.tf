2c9ebb1e5999db4bd2057a5aa7151abd010a9de7
Gary Bowers <garybowers@google.com>
2020-12-15 18:08:45 +0000

    Fix IAM

--- iam.tf
+++ iam.tf
@@ -1,6 +1,18 @@
+
+data "google_project" "project" {
+  project_id = var.vpc_network_project_id
+}
+
+
 resource "google_project_iam_member" "gke-shared-vpc-user-iam" {
   project = var.vpc_network_project_id
   role    = "roles/container.hostServiceAgentUser"
+  member  = "serviceAccount:${google_service_account.cluster-svc.email}"
+}
+
+resource "google_project_iam_member" "gke-shared-vpc-user-iam" {
+  project = var.vpc_network_project_id
+  role    = "roles/compute.networkUser"
   member  = "serviceAccount:${google_service_account.cluster-svc.email}"
 }
 
