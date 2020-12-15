40430c9e4084c9f43dd1ff7da69f94bd42eb796d
Gary Bowers <garybowers@google.com>
2020-12-15 18:29:46 +0000

    Fix IAM

--- iam.tf
+++ iam.tf
@@ -4,13 +4,13 @@ data "google_project" "project" {
 }
 
 
-resource "google_project_iam_member" "gke-shared-vpc-user-iam" {
+resource "google_project_iam_member" "gke-shared-vpc-user-iam-container" {
   project = var.vpc_network_project_id
   role    = "roles/container.hostServiceAgentUser"
   member  = "serviceAccount:${google_service_account.cluster-svc.email}"
 }
 
-resource "google_project_iam_member" "gke-shared-vpc-user-iam" {
+resource "google_project_iam_member" "gke-shared-vpc-user-iam-compute" {
   project = var.vpc_network_project_id
   role    = "roles/compute.networkUser"
   member  = "serviceAccount:${google_service_account.cluster-svc.email}"
