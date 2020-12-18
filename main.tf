566a1cda2c8e3272bd2999a07fa16bbf400a4959
Gary Bowers <garybowers@google.com>
2020-12-18 10:13:38 +0000

    Enable ACM

--- main.tf
+++ main.tf
@@ -7,5 +7,19 @@ resource "google_service_account" "anthos-svc" {
   project      = var.project_id
   account_id   = "anthos-${random_id.postfix.hex}-svc"
   display_name = "anthos-${random_id.postfix.hex}-svc"
+}
 
+module "enable_acm" {
+  source  = "terraform-google-modules/gcloud/google"
+  version = "~> 2.0"
+
+  platform              = "linux"
+  upgrade               = true
+  additional_components = ["alpha"]
+
+  service_account_key_file = var.service_account_key_file
+  create_cmd_entrypoint    = "gcloud"
+  create_cmd_body          = "alpha container hub config-management enable --project ${var.project_id}"
+  destroy_cmd_entrypoint   = "gcloud"
+  destroy_cmd_body         = "alpha container hub config-management disable --force --project ${var.project_id}"
 }
